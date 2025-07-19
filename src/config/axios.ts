6c35c29defb4fe1074d255dc3ff612a37cccb8ad
mujeeb07 <mujeebalp742@gmail.com>
2025-07-19 15:49:01 +0530

    axios api changed to backend

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosinstance = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: `${import.meta.env.BACKEND}`,
     withCredentials: true,
 });
 
